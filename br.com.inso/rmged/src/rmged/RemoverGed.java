c6a2e4c7cca0d0e7eb0ed3af678bc0211a217966
Glauber Duma <binho_duma@hotmail.com>
2016-03-08 15:37:59 -0300

    Commit inicial

---
+++ br.com.inso/rmged/src/rmged/RemoverGed.java
@@ -0,0 +1,22 @@
+package rmged;
+
+import java.io.File;
+
+public class RemoverGed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if (args.length > 0)
+		{
+			File arquivo = new File(args[0]);
+			
+			if (arquivo.exists())   
+			{
+				arquivo.delete();
+			}
+		}
+
+	}
+
+}
